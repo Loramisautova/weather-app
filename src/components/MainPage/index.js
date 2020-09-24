68c727b0caa071949e2be1a9a4f7b0a3984e10f7
Lora Misautova <loramisautova@Loras-MacBook-Air.local>
2020-09-24 13:16:37 +0400

    Added input field and submit button.

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-export const MainPage = () => {
-  return (
-    <div >
-     Main Page.
-    </div>
-  );
-}
+export class MainPage extends React.PureComponent {
+
+    state = {
+        query: "",
+        text: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({ query: e.target.value });
+    }
+
+    handleSubmit = () => {
+        this.setState({ text: this.state.query });
+    }
+
+    render () {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    placeholder="Search for Location / City"
+                />
+                <button 
+                    onClick={this.handleSubmit}
+                >   
+                Search
+                </button>
+                <div> {this.state.text} </div>
+          </div>
+        );
+    }
+  }
