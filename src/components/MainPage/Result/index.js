d4d29fef70818170f3fb31102286300edf0930a4
Lora Misautova <loramisautova@Loras-MacBook-Air.local>
2020-10-08 12:56:34 +0400

    Updated.

--- src/components/MainPage/Result/index.js
+++ src/components/MainPage/Result/index.js
@@ -5,17 +5,31 @@ import { Grid } from "@material-ui/core";
 
 export class Result extends React.PureComponent {
 
+    renderContent (result) {
+        switch (result.cod) { 
+            case 200: { 
+                return (<> 
+                        <Grid container item xs={3}> {result.name}, {result.sys.country} </Grid> 
+                        <Grid item xs={3}> {Math.round(result.main.temp)}&#176; </Grid> 
+                    </>
+                );
+            }           
+            case "404": { 
+                return (
+                    <Grid item xs={6}> Address Not Found </Grid>
+                );
+            }  
+            default: 
+                return null; 
+        };
+    }
+
     render() {
         const { result } = this.props;
         return (
             <div className='visibility-panel'>
                 <Grid container justify='space-between'>
-                    {Boolean(result) && 
-                        <>
-                            <Grid container item xs={3}> {result.name}, {result.sys.country} </Grid>
-                            <Grid item xs={3}> {Math.round(result.main.temp)}&#176; </Grid>
-                        </>
-                    }
+                   {Boolean(result) && this.renderContent(result)}
                 </Grid>
             </div>
         )
--- src/components/MainPage/SearchBar/index.js
+++ src/components/MainPage/SearchBar/index.js
@@ -1,44 +1,79 @@
 import React from 'react';
+import "./index.css";
 
-import {Button, TextField, Grid} from "@material-ui/core";
+import {Button, Grid, TextField} from "@material-ui/core";
+import { Autocomplete } from '@material-ui/lab';
 
 export class SearchBar extends React.PureComponent {
    
     state = {
-        address: ''
+        address: '',
+        suggestions: []
     }
     handleSubmit = () => {
         this.props.onSearch(this.state.address);
     };
 
     handleChange = (e) => {
-        this.setState({ address: e.target.value });
-      };
+        this.setState({ address: e.target.value
+        }, async () => {
+            const API_KEY = '4bf4c660-0729-11eb-b357-a938736de318';
+            const { address } = this.state;
+
+            if ( address.length >= 3 ) {
+                try {
+                    const request = await fetch(`https://app.geocodeapi.io/api/v1/autocomplete?apikey=${API_KEY}&text=${address}&size=10`);
+                    const result = await request.json();
+                    console.log(result)
+    
+                    const suggestions = result.features.map((item) => item.properties.label);   
+    
+                    this.setState({
+                        suggestions
+                    });
+    
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        });
+    }
 
     render () {
+        const {suggestions} = this.state;
         return (
             <Grid container alignItems="center">
                 <Grid container item xs={10} alignItems="center">
-                    <TextField
-                        type="text" 
-                        id="standard-basic"
-                        color="primary"
-                        label="Enter city" 
-                        value={this.state.address}
-                        onChange={this.handleChange}
-                        fullWidth
-                    />
+                    <div className='search-text-field'>
+                        <Autocomplete 
+                            freeSolo
+                            id="free-solo-2-demo"
+                            disableClearable
+                            options={suggestions}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Search city"
+                                    margin="normal"
+                                    variant="outlined"
+                                    onChange={this.handleChange}
+                                    InputProps={{ ...params.InputProps, type: 'search' }}
+                                />
+                            )}
+                        />
+                    </div>
                 </Grid>
-                <Grid item xs={2}>
-                    <Button 
-                        variant="outlined" 
-                        size="small" 
-                        color="primary"
-                        onClick={this.handleSubmit}
-                        fullWidth
-                    >
-                        Search
-                    </Button>
+                <Grid container item xs={2} alignItems="center">
+                    <div className='search-btn'>
+                        <Button 
+                            variant="contained"
+                            size="large"
+                            color="primary"
+                            onClick={this.handleSubmit}
+                        >
+                            Search
+                        </Button>
+                    </div>
                 </Grid>
             </Grid>
         );
