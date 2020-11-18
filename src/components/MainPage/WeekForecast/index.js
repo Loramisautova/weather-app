c02ec96444622458c8024c2104548b8f1581e989
Lora Misautova <loramisautova@Loras-MacBook-Air.local>
2020-11-19 02:44:45 +0400

    Add unit switcher.

---
+++ src/components/MainPage/WeekForecast/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box } from "@material-ui/core";
+
+import { ForecastCard } from '../ForecastCard';
+
+export const WeekForecast = ({ forecast, fahreingheit }) => {
+  const weekDays = forecast.slice(1);
+
+  return (
+    <Box display = "flex" flexDirection = "row" justifyContent="space-evenly" p={3}>
+        {
+          forecast &&
+            weekDays.map((day) =>
+                <ForecastCard
+                    key={day.dt}
+                    day={day}
+                    fahreingheit={fahreingheit}
+                />
+            )
+        }
+    </Box>
+  );
+}
+
